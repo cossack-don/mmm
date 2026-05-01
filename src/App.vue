092a9b9c12f120a56aa7b991a9aa8df49c658210
cossack-done <ser90593696.zhuravlev@yandex.ru>
2026-05-01 13:34:39 +0300

    feat: create conntect api - check-health

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import { onMounted } from "vue";
+
+const checkHealthApi = async () => {
+  const res = await fetch("http://localhost:3000/check-health");
+  const json = await res.json();
+
+  console.log("check-health-API", json);
+};
+onMounted(() => {
+  checkHealthApi();
+});
 </script>
 
 <template>
